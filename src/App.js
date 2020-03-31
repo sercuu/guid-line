8013949583fd067b064f70b67ac79d1597105d4b
Sercan Turkoglu <sercanturkoglu@Sercan-MacBook-Pro.local>
2020-03-31 11:33:10 +0300

    guid line fixed

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './index.scss';
 
 function App() {
     return (
         <div className="App">
             <header className="App-header">
-                <img
-                  src={logo}
-                  className="App-logo"
-                  alt="logo"
-                />
+
                 <p>
                     Edit
-                    {' '}
+
                     <code>src/App.js</code>
                     {' '}
                     and save to reload.
