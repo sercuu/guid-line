4c6d99f4ad5d2bb08da362d77f0911c2712af81f
Sercan Turkoglu <sercanturkoglu@Sercan-MacBook-Pro.local>
2020-03-31 11:36:12 +0300

    husky test

--- src/App.js
+++ src/App.js
@@ -9,9 +9,7 @@ function App() {
 
                 <p>
                     Edit
-
                     <code>src/App.js</code>
-                    {' '}
                     and save to reload.
                 </p>
                 <a
